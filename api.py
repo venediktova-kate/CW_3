151d2dda3685ce7a842f99ebb755cdfa200602fd
venediktova-kate <venediktova.kat@gmail.com>
2023-03-23 22:58:18 +0700

    new commit

--- api.py
+++ api.py
@@ -6,13 +6,13 @@ import logging
 api_bp = Blueprint('api', __name__)
 
 
-@api_bp.route('post/')
+@api_bp.route('/post/')
 def main_page():
     posts = utils.get_posts_all()
     return jsonify(posts)
 
 
-@api_bp.route('posts/<int:post_id>')
+@api_bp.route('/posts/<int:post_id>')
 def post_page(post_id):
     post = utils.get_post_by_pk(post_id)
     comments = utils.get_comments_by_post_id(post_id)
